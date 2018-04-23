2b6c55e2bf9c700de350ae478f755cfc1badbcb3
Ben Bangert <ben@groovie.org>
2018-04-23 15:16:37 -0700

    chore: rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,15 @@
 #[macro_use]
 extern crate serde_derive;
 
-extern crate rand;
 extern crate base64;
 extern crate byteorder;
 extern crate openssl;
+extern crate rand;
 
 use byteorder::{ReadBytesExt, WriteBytesExt};
 use std::io::{Cursor, Read};
 use std::time;
 use rand::Rng;
-
 
 const MAX_CLOCK_SKEW: u64 = 60;
 
@@ -27,14 +26,17 @@ impl Fernet {
     pub fn new(key: &str) -> Fernet {
         let key = base64::decode_config(key, base64::URL_SAFE).unwrap();
         assert_eq!(key.len(), 32);
-        Fernet{
+        Fernet {
             signing_key: key[..16].to_vec(),
             encryption_key: key[16..].to_vec(),
         }
     }
 
     pub fn encrypt(&self, data: &[u8]) -> String {
-        let current_time = time::SystemTime::now().duration_since(time::UNIX_EPOCH).unwrap().as_secs();
+        let current_time = time::SystemTime::now()
+            .duration_since(time::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
         let mut iv = vec![0; 16];
         rand::OsRng::new().unwrap().fill_bytes(&mut iv);
         return self._encrypt_from_parts(data, current_time, &iv);
@@ -45,17 +47,20 @@ impl Fernet {
             openssl::symm::Cipher::aes_128_cbc(),
             &self.encryption_key,
             Some(iv),
-            data
+            data,
         ).unwrap();
 
         let mut result = Vec::new();
         result.push(0x80);
-        result.write_u64::<byteorder::BigEndian>(current_time).unwrap();
+        result
+            .write_u64::<byteorder::BigEndian>(current_time)
+            .unwrap();
         result.extend_from_slice(iv);
         result.extend_from_slice(&ciphertext);
 
         let hmac_pkey = openssl::pkey::PKey::hmac(&self.signing_key).unwrap();
-        let mut hmac_signer = openssl::sign::Signer::new(openssl::hash::MessageDigest::sha256(), &hmac_pkey).unwrap();
+        let mut hmac_signer =
+            openssl::sign::Signer::new(openssl::hash::MessageDigest::sha256(), &hmac_pkey).unwrap();
         hmac_signer.update(&result).unwrap();
 
         result.extend_from_slice(&hmac_signer.sign_to_vec().unwrap());
@@ -64,28 +69,36 @@ impl Fernet {
     }
 
     pub fn decrypt(&self, token: &str) -> Result<Vec<u8>, DecryptionError> {
-        let current_time = time::SystemTime::now().duration_since(time::UNIX_EPOCH).unwrap().as_secs();
+        let current_time = time::SystemTime::now()
+            .duration_since(time::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
         return self._decrypt_at_time(token, None, current_time);
     }
 
     // TODO: add decrypt_with_ttl()
 
-    fn _decrypt_at_time(&self, token: &str, ttl: Option<u64>, current_time: u64) -> Result<Vec<u8>, DecryptionError> {
+    fn _decrypt_at_time(
+        &self,
+        token: &str,
+        ttl: Option<u64>,
+        current_time: u64,
+    ) -> Result<Vec<u8>, DecryptionError> {
         let data = match base64::decode_config(token, base64::URL_SAFE) {
             Ok(data) => data,
-            Err(_) => return Err(DecryptionError)
+            Err(_) => return Err(DecryptionError),
         };
 
         let mut input = Cursor::new(data);
 
         match input.read_u8() {
-            Ok(0x80) => {},
-            _ => return Err(DecryptionError)
+            Ok(0x80) => {}
+            _ => return Err(DecryptionError),
         }
 
         let timestamp = match input.read_u64::<byteorder::BigEndian>() {
             Ok(value) => value,
-            Err(_) => return Err(DecryptionError)
+            Err(_) => return Err(DecryptionError),
         };
 
         if let Some(ttl) = ttl {
@@ -108,12 +121,15 @@ impl Fernet {
         if rest.len() < 32 {
             return Err(DecryptionError);
         }
-        let ciphertext = &rest[..rest.len()-32];
+        let ciphertext = &rest[..rest.len() - 32];
         let hmac = &rest[rest.len() - 32..];
 
         let hmac_pkey = openssl::pkey::PKey::hmac(&self.signing_key).unwrap();
-        let mut hmac_signer = openssl::sign::Signer::new(openssl::hash::MessageDigest::sha256(), &hmac_pkey).unwrap();
-        hmac_signer.update(&input.get_ref()[..input.get_ref().len() - 32]).unwrap();
+        let mut hmac_signer =
+            openssl::sign::Signer::new(openssl::hash::MessageDigest::sha256(), &hmac_pkey).unwrap();
+        hmac_signer
+            .update(&input.get_ref()[..input.get_ref().len() - 32])
+            .unwrap();
         let expected_hmac = hmac_signer.sign_to_vec().unwrap();
         if !openssl::memcmp::eq(&expected_hmac, hmac) {
             return Err(DecryptionError);
@@ -123,10 +139,10 @@ impl Fernet {
             openssl::symm::Cipher::aes_128_cbc(),
             &self.encryption_key,
             Some(&iv),
-            ciphertext
+            ciphertext,
         ) {
             Ok(value) => value,
-            Err(_) => return Err(DecryptionError)
+            Err(_) => return Err(DecryptionError),
         };
 
         return Ok(plaintext);
@@ -170,11 +186,18 @@ mod tests {
     #[test]
     fn test_generate_vectors() {
         // TODO: the vector file shouldn't be in this directory
-        let vectors: Vec<GenerateVector> = serde_json::from_str(include_str!("generate.json")).unwrap();
+        let vectors: Vec<GenerateVector> =
+            serde_json::from_str(include_str!("generate.json")).unwrap();
 
         for v in vectors {
             let f = Fernet::new(v.secret);
-            let token = f._encrypt_from_parts(v.src.as_bytes(), chrono::DateTime::parse_from_rfc3339(v.now).unwrap().timestamp() as u64, &v.iv);
+            let token = f._encrypt_from_parts(
+                v.src.as_bytes(),
+                chrono::DateTime::parse_from_rfc3339(v.now)
+                    .unwrap()
+                    .timestamp() as u64,
+                &v.iv,
+            );
             assert_eq!(token, v.token);
         }
     }
@@ -186,7 +209,13 @@ mod tests {
 
         for v in vectors {
             let f = Fernet::new(v.secret);
-            let decrypted = f._decrypt_at_time(v.token, Some(v.ttl_sec), chrono::DateTime::parse_from_rfc3339(v.now).unwrap().timestamp() as u64);
+            let decrypted = f._decrypt_at_time(
+                v.token,
+                Some(v.ttl_sec),
+                chrono::DateTime::parse_from_rfc3339(v.now)
+                    .unwrap()
+                    .timestamp() as u64,
+            );
             assert_eq!(decrypted, Ok(v.src.as_bytes().to_vec()));
         }
     }
@@ -194,11 +223,18 @@ mod tests {
     #[test]
     fn test_invalid_vectors() {
         // TODO: the vector file shouldn't be in this directory
-        let vectors: Vec<InvalidVector> = serde_json::from_str(include_str!("invalid.json")).unwrap();
+        let vectors: Vec<InvalidVector> =
+            serde_json::from_str(include_str!("invalid.json")).unwrap();
 
         for v in vectors {
             let f = Fernet::new(v.secret);
-            let decrypted = f._decrypt_at_time(v.token, Some(v.ttl_sec), chrono::DateTime::parse_from_rfc3339(v.now).unwrap().timestamp() as u64);
+            let decrypted = f._decrypt_at_time(
+                v.token,
+                Some(v.ttl_sec),
+                chrono::DateTime::parse_from_rfc3339(v.now)
+                    .unwrap()
+                    .timestamp() as u64,
+            );
             assert_eq!(decrypted, Err(DecryptionError));
         }
     }
@@ -208,9 +244,18 @@ mod tests {
         let f = Fernet::new(&base64::encode_config(&vec![0; 32], base64::URL_SAFE));
 
         // Invalid version byte
-        assert_eq!(f.decrypt(&base64::encode_config(b"\x81", base64::URL_SAFE)), Err(DecryptionError));
+        assert_eq!(
+            f.decrypt(&base64::encode_config(b"\x81", base64::URL_SAFE)),
+            Err(DecryptionError)
+        );
         // Timestamp too short
-        assert_eq!(f.decrypt(&base64::encode_config(b"\x80\x00\x00\x00", base64::URL_SAFE)), Err(DecryptionError));
+        assert_eq!(
+            f.decrypt(&base64::encode_config(
+                b"\x80\x00\x00\x00",
+                base64::URL_SAFE
+            )),
+            Err(DecryptionError)
+        );
         // Invalid base64
         assert_eq!(f.decrypt("\x00"), Err(DecryptionError));
     }
@@ -219,11 +264,7 @@ mod tests {
     fn test_roundtrips() {
         let f = Fernet::new(&base64::encode_config(&vec![0; 32], base64::URL_SAFE));
 
-        for val in [
-            b"".to_vec(),
-            b"Abc".to_vec(),
-            b"\x00\xFF\x00\x00".to_vec(),
-        ].into_iter() {
+        for val in [b"".to_vec(), b"Abc".to_vec(), b"\x00\xFF\x00\x00".to_vec()].into_iter() {
             assert_eq!(f.decrypt(&f.encrypt(&val)), Ok(val.clone()));
         }
     }
